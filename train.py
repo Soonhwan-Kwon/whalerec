a2d88d812b1e6cca6e2525ab0702b70a74f6271a
crowmagnumb <crowmagnumb@gmail.com>
2018-08-06 11:10:23 -0700

    proper boolean flag

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import modelUtils
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--test', action="store", type=int)  # Number of records to test with
+parser.add_argument('-t', '--test', action='store_true')
 parser.add_argument('-d', '--datadir', dest='datadir')
 args = parser.parse_args()
 
